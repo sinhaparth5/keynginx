83f2fc164e0d5d930e138fd50d269563a0a440b5
Parth Sinha <sinhaparth555@gmail.com>
2025-08-23 17:03:23 +0100

    added root file

--- cmd/root.go
+++ cmd/root.go
@@ -2,9 +2,26 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "keynginx",
+	Short: "KeyNginx - SSL Certificate Generator",
+	Long: `KeyNginx is a CLI tool for generatoring SSL certificates.
+	This is Phase 1 - focused on core certificates generation functionality`,
+	Version: getVersion(),
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "verbose output")
+}
+
+func getVersion() string {
+	return fmt.Sprintf("%s (commit: %s, built: %s)", Version, GitCommit, BuildTime)
+}
